871579106c01f99ccb9089fe265c97ca2f31e111
Your Name <kholoudbatis@gmail.com>
2022-01-15 13:10:49 +0300

    hotfix

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ Route::get('/home', 'HomeController@index')->name('home')->middleware('verified'
 Route::get('/', function(){
 
 return view('/home');
-
 });
 Route :: get('/dashboard ' , function(){
+
  return 'dashboard';
 });
